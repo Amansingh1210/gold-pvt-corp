25deda883eb6cb5aef1a0d81a0075c06908efc8e
Aman Singh <singhaman38809@gmail.com>
2024-05-13 14:27:49 +0530

    react-router setup complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import PrivateRoute from './Components/PrivateRoute'
 import Navbar from './Components/Navbar'
 import Slider from './Components/Slider'
 import Products from './Components/Products'
@@ -10,6 +9,7 @@ import About from './Components/About'
 import Footer from './Components/Footer'
 import Team from './Components/Team'
 import Mission from './Components/Mission'
+import Laboratory from './Components/Laboratory'
 
 function App() {
   return (
@@ -25,6 +25,10 @@ function App() {
             <Gallery />
           </div>
           } />
+          <Route path='/about' element={<About />} />
+          <Route path='/mission' element={<Mission />} />
+          <Route path='/team' element={<Team />} />
+          <Route path='/lab' element={<Laboratory />} />
         </Routes>
         <Footer />
       </BrowserRouter>
